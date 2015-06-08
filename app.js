f59f7472cfe6c7794176ba23a69daae0d74b24cf
Sulka Haro <sulka@sulka.net>
2015-06-09 00:05:22 +0300

    compress all requests

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ function create (env, ctx) {
   app.set('title', appInfo);
   app.enable('trust proxy'); // Allows req.secure test on heroku https connections.
 
-  app.use(compression({filter: shouldCompress}));
+  app.use(compression()); // {filter: shouldCompress}));
 
   function shouldCompress(req, res) {
       //TODO: return false here if we find a condition where we don't want to compress
